2187d88f7145958110c9f182b01ca25fedebe5a2
Ji-Won Kim <jiwonkim89@gmail.com>
2021-12-24 22:55:53 -0800

    app list

--- src/App.js
+++ src/App.js
@@ -2,57 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 import { render } from "@testing-library/react";
+import React from 'react';
 
-<h1>To Do List</h1>
-
-class ToDoList React.Component {
-    constructor(props) {
-        super(props);
-        this.state=  [
-            {status: false,
-             description: '' }
-        ]
-    }
-
-    addToList(newText) {
-        return (this.state.push({
-            status: false,
-            description: newText
-        }))
-    }
-
-    }
-    render() {
-        <form id="newText">
-            <input type="text" value={this.state.value} onChange={this.addToList}>
-        </form>
-        return (
-            <button onClick={this.addToList}>
-            '+'
-            </button>
-        )
-    }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''}
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {    this.setState({value: event.target.value});  }
+  render() {
+    return (
+      <form id="newText">
+        <input type="text" value= {this.state.value} onChange={this.handleChange}/>
+      </form>
+    )
+  }
 }
-<input type="text" value={this.state.value} onChange={this.handleChange} />
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
 export default App;
